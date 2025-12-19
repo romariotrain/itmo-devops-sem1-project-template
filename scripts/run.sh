ca25234ef610fb2cb54e3b9acc458067ae8b3095
Roman <romamorozevich@yandex.ru>
2025-12-19 14:17:16 +0000

    run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-# Скрипт для запуска приложения
-
 set -e
 
 echo "=== Запуск приложения ==="
 
-# Проверка, что PostgreSQL запущен
 if ! sudo service postgresql status > /dev/null 2>&1; then
     echo "Запуск PostgreSQL..."
     sudo service postgresql start
 fi
 
-# Проверка подключения к базе данных
 if ! PGPASSWORD='val1dat0r' psql -h localhost -U validator -d project-sem-1 -c '\q' > /dev/null 2>&1; then
     echo "Ошибка: не удается подключиться к базе данных."
     echo "Запустите сначала скрипт prepare.sh"
@@ -21,15 +17,15 @@ fi
 
 echo "База данных доступна!"
 
-# Сборка и запуск приложения
 echo "Сборка приложения..."
 if ! go build -o server main.go; then
-    echo "Ошибка в сборке приложения"
+    echo "Application build error"
+    echo "qweeqw"
     exit 1
 fi
 
 if [ ! -f "./server" ]; then
-    echo "Файл не создан"
+    echo "not created"
     exit 1
 fi
 
